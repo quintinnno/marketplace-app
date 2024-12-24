eac3fadcac617d934283d7558237cf482891f2dd
José Quintinno <jose.quintinno@outlook.com.br>
2024-12-24 18:46:26 -0300

    🎯 feature: Implementar componente de Tabmenu

---
+++ app/(tabs)/(home)/stores.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function StoresScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "dark" }}>Stores</Text>
+    </View>
+  );
+}
