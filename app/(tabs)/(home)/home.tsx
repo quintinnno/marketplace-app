8e663487451c20a3191b134cac8d80543b55b63f
José Quintinno <jose.quintinno@outlook.com.br>
2024-12-24 18:07:07 -0300

    🎯 feature: Implementar componente de Tabmenu

--- app/(tabs)/(home)/home.tsx
+++ app/(tabs)/(home)/home.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ color: "dark" }}>Home</Text>
+      <Link href={"/(tabs)/(home)/search"} style={{ color: "#000" }}>Search</Link>
     </View>
   );
 }
